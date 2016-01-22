1d2029d4c5e0fb724b6985e675a1cf444d74c1df
xylcbd <dahanhbm@163.com>
2016-01-22 18:37:12 +0800

    first commit.

---
+++ OverlayChineseOnImage/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include "i18nText.h"
+
+int main()
+{
+	const auto srcFile = R"(D:\模糊判断调查\images\u-20151213-5d142666-489d-4309-a355-2bbdf45a701b.png)";
+	const auto srcImg = cv::imread(srcFile);
+	if (!srcImg.data)
+	{
+		std::cerr << "can not open file " << srcFile << std::endl;
+		return -1;
+	}
+	i18nText paiter;
+	const auto font = R"(c:\windows\fonts\stsong.ttf)";
+	bool success = paiter.setFont(font);
+	if (!success)
+	{
+		std::cerr << "can not load font " << font << std::endl;
+		return -1;
+	}
+	int x = srcImg.cols;
+	while (true)
+	{
+		auto shImg = srcImg.clone();
+		paiter.putText(shImg, L"你好啊，我这里是中国上海学霸君！", cv::Point(x, 80), 255);
+		cv::imshow("shImg", shImg);
+		cv::waitKey(10);
+		x-=3;
+		if (x < 100)
+		{
+			x = shImg.cols;
+		}
+	}	
+	return 0;
+}
